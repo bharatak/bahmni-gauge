0aff0eb8a5df7bc74e8460a4ef014e8a4a714cb2
bharatak <bharatak@thoughtworks.com>
2018-05-10 15:01:55 +0530

    Command to start gauge docker container

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 docker run -v /var/go/.m2:/root/.m2:rw -v $PWD:/gauge -i bharatak/docker-gauge-chromedriver:chromedriver-2.34 -- sh run.sh
+chown -R go:go bahmni-gauge-default
