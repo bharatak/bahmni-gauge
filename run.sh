1ebf66eb6a68bf52392dc0cd2a076bf7c450d82b
bharatak <bharatak@thoughtworks.com>
2018-05-10 15:45:51 +0530

    Command to start gauge docker container

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 nohup Xvfb :99 -screen 0 1024x768x24 &
 export DISPLAY=:99
-mvn clean install
-echo "The env is $env"
+# mvn clean install
+echo "The env in run.sh $ENV"
 # cd bahmni-gauge-default/ && mvn install -Denv=$ENV -Dtags=$TAGS
