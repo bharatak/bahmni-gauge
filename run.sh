63bd2516a90e6a80fef0c6f0d8bbb1def3728da7
bharatak <bharatak@thoughtworks.com>
2018-05-10 14:48:12 +0530

    Command to start gauge docker container

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 nohup Xvfb :99 -screen 0 1024x768x24 &
 export DISPLAY=:99
 mvn clean install
-echo "Bahmni URL is $BAHMNI_GAUGE_APP_URL"
-cd bahmni-gauge-default/ && mvn install -Denv=ci -Dtags=sanity1 -DBAHMNI_GAUGE_APP_URL=$BAHMNI_GAUGE_APP_URL
+cd bahmni-gauge-default/ && mvn install -Denv=qa04 -Dtags=sanity1
