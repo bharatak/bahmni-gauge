3c3ad4f810993d883b73f78eb0809c67953bc8c3
bharatak <bharatak@thoughtworks.com>
2018-05-10 12:57:28 +0530

    Command to start gauge docker container

--- run-docker.sh
+++ run-docker.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-BASEDIR=${dirname $0}
-echo "The base directory is $BASEDIR"
-
-docker run -v /var/go/.m2:/root/.m2:rw -v $BASEDIR/bahmni-gauge:/gauge -i bharatak/docker-gauge-chromedriver:chromedriver-2.34 -- sh run.sh
+docker run -v /var/go/.m2:/root/.m2:rw -v $PWD:/gauge -i bharatak/docker-gauge-chromedriver:chromedriver-2.34 -- sh run.sh
