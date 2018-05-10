a061c3e86d72f4f26882f4c6865928068d420117
bharatak <bharatak@thoughtworks.com>
2018-05-10 15:39:20 +0530

    Command to start gauge docker container

--- run-docker.sh
+++ run-docker.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-docker run -v /var/go/.m2:/root/.m2:rw -v $PWD:/gauge -i bharatak/docker-gauge-chromedriver:chromedriver-2.34 -- sh run.sh
+docker run -v /var/go/.m2:/root/.m2:rw -v $PWD:/gauge -i bharatak/docker-gauge-chromedriver:chromedriver-2.34 -- sh run.sh $ENV
 #Hack. The html-report executable is symlinked as a root user.  
 #so, removing it so that the artifact is accessible from gocd server
 rm -f bahmni-gauge-default/reports/html-report/html-report
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 nohup Xvfb :99 -screen 0 1024x768x24 &
 export DISPLAY=:99
 mvn clean install
-echo "The env is $ENV and tags are $TAGS"
+echo "The env is $1"
 # cd bahmni-gauge-default/ && mvn install -Denv=$ENV -Dtags=$TAGS
